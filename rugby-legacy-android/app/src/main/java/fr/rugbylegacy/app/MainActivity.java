a7c07d31160ee710a54495a4f733ef197da84dd0
jumerca <152607703+jumerca@users.noreply.github.com>
2026-09-10 12:54:56 +0200

    Build Rugby Legacy Android APK

---
+++ rugby-legacy-android/app/src/main/java/fr/rugbylegacy/app/MainActivity.java
@@ -0,0 +1,59 @@
+package fr.rugbylegacy.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setStatusBarColor(Color.rgb(6,17,13));
+        getWindow().setNavigationBarColor(Color.rgb(6,17,13));
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(6,17,13));
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        setContentView(webView);
+
+        if (savedInstanceState == null) {
+            webView.loadUrl("file:///android_asset/www/index.html");
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
